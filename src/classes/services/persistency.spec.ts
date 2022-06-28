9a53e71c1e78ee15cd1b0ea1427920470f5842f4
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-06-28 15:03:03 -0300

    test: adding pesistency tests

--- src/classes/services/persistency.spec.ts
+++ src/classes/services/persistency.spec.ts
@@ -1,5 +1,29 @@
-it("Should test something", () => {
-  const number = 1;
+import { Persistency } from "./persistency";
 
-  expect(number).toBe(1);
+describe("Persistency", () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it("Should return undefined", () => {
+    // system under test
+    const sut = new Persistency();
+
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it("Should call console.log once", () => {
+    // system under test
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, "log");
+
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalled();
+  });
+
+  it("Should call console.log with 'Pedido salvo com sucesso!'", () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, "log");
+
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith("Pedido salvo com sucesso!");
+  });
 });
